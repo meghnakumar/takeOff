02ea7376030372f3b4c24482d483b9ed55c9d5cf
sharad <sh244392.dal.ca>
2022-07-18 23:14:05 -0300

    added error messages for wrong credential

--- frontend/src/components/Registration/Login.js
+++ frontend/src/components/Registration/Login.js
@@ -99,8 +99,13 @@ export default function SignupForm(props) {
         return "noerror";
   }
 
+<<<<<<< HEAD
   const [success, showSuccess] = React.useState();
   const [error, showError] = React.useState();
+=======
+  const [snackBox, showSnackBox] = React.useState();
+  const [errorSnackBox, showErrorSnackBox] = React.useState();
+>>>>>>> cb5efad (added error messages for wrong credential)
 
   const loginSuccessful = () => {
     showSuccess(true);
@@ -125,10 +130,18 @@ export default function SignupForm(props) {
 
 
   const loginFailed = () => {
+<<<<<<< HEAD
     showError(true);
       setTimeout(() => {
         showError(false);
       }, 500);
+=======
+    console.log("login failed")
+    showErrorSnackBox(true);
+      setTimeout(() => {
+        showErrorSnackBox(false);
+      }, 1000);
+>>>>>>> cb5efad (added error messages for wrong credential)
     }
 
   const SaveUserDetails = () => {   
@@ -145,9 +158,8 @@ export default function SignupForm(props) {
           }else{
             loginFailed();
           }
-          
-          
-
+          }).catch(err => {
+            loginFailed();
           });
     } 
   }
@@ -183,11 +195,19 @@ export default function SignupForm(props) {
             <br></br>
 
             {
+<<<<<<< HEAD
             success ?
               <Snackbox message="User logged in succesfully" severity="success" /> : null
             }
             {
               error ? 
+=======
+            snackBox ?
+              <Snackbox message="User logged in succesfully" severity="success" /> : null
+            }
+            {
+            errorSnackBox ?
+>>>>>>> cb5efad (added error messages for wrong credential)
               <Snackbox message="Wrong credential!" severity="error" /> : null
             }
 
